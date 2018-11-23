87a0ccc17651375c88dadc0be6efa17e69510185
XuanmingChen <xc222an@student.lnu.se>
2018-11-24 00:10:14 +0100

    REFACTOR

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,14 +1,14 @@
 import java.util.NoSuchElementException;
 
-import java.util.NoSuchElementException;
 
 public class Vertex<Type> {
     public Type data;
 
 
     public void setData(Type data) {
-        this.data = data;
         if(data == "") throw new NoSuchElementException();
+        this.data = data;
+
     }
 
 
