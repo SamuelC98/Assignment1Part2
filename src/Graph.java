2ad0e052d8e68d36f7d4f6111154462d8ad43975
XuanmingChen <xc222an@student.lnu.se>
2018-11-24 23:10:02 +0100

    REFACTOR

--- src/Graph.java
+++ src/Graph.java
@@ -1,12 +1,13 @@
 
 public class Graph<Type> {
     SeqList<Vertex<Type>> verticesList = new SeqList<Vertex<Type>>();
-    int numVertex;
+
 
     public Graph() {
 
     }
 
+    //get data in graph
     public Type getVertexData(int number){
         return verticesList.get(number).getData();
     }
