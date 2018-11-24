ff7986bcf88f553d3584d96e35a494f711234aee
XuanmingChen <xc222an@student.lnu.se>
2018-11-24 11:42:29 +0100

    REFACTOR

--- src/EdgeNode.java
+++ src/EdgeNode.java
@@ -1,18 +1,14 @@
 public class EdgeNode {
     public int dest;
-
-
     public int distance;//
     public int fee;
-
     public EdgeNode next;
 
-    public EdgeNode(int dest, int fee, int distance) {this.dest = dest;this.distance = distance;
+    public EdgeNode(int dest, int fee, int distance) {
+        this.dest = dest;this.distance = distance;
         this.fee = fee;
 
     }
 
-    public EdgeNode(int dest, int fee, int distance, EdgeNode next) {
 
-    }
 }
