63074b7f425227837878b98aecbf09a130394e32
XuanmingChen <xc222an@student.lnu.se>
2018-11-29 09:05:49 +0100

    RED

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    private static CityCheck cityCheck = new CityCheck();
+    private static ReadFile rf1 = new ReadFile();
+    private static Graph graph1 = new Graph();
+    private static Graph Europe = new Graph();
+    private static Vertex temp = new Vertex();
+    private static String data1 = "London";
+    private static String data2 = "Skopja";
+
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
