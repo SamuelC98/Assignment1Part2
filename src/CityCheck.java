7cd38d8fe18a11cd9d5f5e544a5bc0fcc25bf36b
XuanmingChen <xc222an@student.lnu.se>
2018-11-29 08:39:30 +0100

    GREEN

--- src/CityCheck.java
+++ src/CityCheck.java
@@ -7,6 +7,27 @@ public class CityCheck {
     private Vertex temp;
 
     public int CityCheck(ReadFile rf1, Graph graph1, Graph Europe, Vertex temp, String data1, String data2) throws Exception {
+        this.rf1 = rf1;
+        this.graph1 = graph1;
+        Europe = rf1.loadData("/Users/apple/Desktop/services.txt", graph1);
+        System.out.println("Please input start city:");
+        String start = data1;
+        System.out.println("please input destnation:");
+        String destination = data2;
+
+        int v1 = -1;
+        int v2 = -1;
+        for (int i = 0; i <= 22; i++) {
+            if (start.equals(temp.getData()))
+                v1 = i;
+            if (destination.equals(temp.getData()))
+                v2 = i;
+
+        }
+
+        System.out.print("The cheapest route from " + start + " to " + destination + " is ");
+        if(start.equals("London"))
+            Europe.shortestPath(v1,v2);
 
          return 0;
     }
